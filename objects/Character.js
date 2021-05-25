d0385027dddf370316d8a6e5f2030540ccc3d7cf
Tobias <tobiasvpini@gmail.com>
2021-05-25 16:22:35 +0200

    reorganization of modules

---
+++ objects/Character.js
@@ -0,0 +1,10 @@
+export default class Character{
+    constructor(id, name, status, species, location, image){
+        this.id = id;
+        this.name = name;
+        this.status = status;
+        this.species = species;
+        this.location = location;
+        this.image = image;
+    }
+}
