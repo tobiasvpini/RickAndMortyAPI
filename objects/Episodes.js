efaddf6a39451744bcad34c55abede9d76421f2a
Tobias <tobiasvpini@gmail.com>
2021-05-25 16:23:00 +0200

    Episodes object created

---
+++ objects/Episodes.js
@@ -0,0 +1,8 @@
+export default class Episodes{
+    constructor(id, air_date, episode, name){
+        this.id = id;
+        this.air_date = air_date;
+        this.episode = episode;
+        this.name = name;
+    }
+}
