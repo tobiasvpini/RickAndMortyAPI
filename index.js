075299417401a4bae837c5db50101b0f81435326
Tobias <tobiasvpini@gmail.com>
2021-05-25 16:24:10 +0200

    V1.02 getting episodes from API

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import Character from "./Character.js";
+import Character from "./objects/Character.js";
+import Episodes from "./objects/Episodes.js";
 
 const baseURL  = "https://rickandmortyapi.com/api";
 
@@ -6,7 +7,7 @@ let fragment = new DocumentFragment();
 
 document.querySelector("#obtainPJ").addEventListener("click", async () => {
     const charactersContainer = document.querySelector("#all");
-
+    
     const response = await fetch(baseURL);
     const data = await response.json();
     const characters = await data.characters;
@@ -19,7 +20,7 @@ document.querySelector("#obtainPJ").addEventListener("click", async () => {
     const obtainedCharacters = await function (dataCharacters) {                
         charactersArray = loadingCharactersObject(dataCharacters.results);
         for(let i = 0; i < charactersArray.length; i++){
-            fragment.append(createHTMLElements(charactersArray[i]));
+            fragment.append(createHTMLCharactersElements(charactersArray[i]));
         }
         charactersContainer.append(fragment);
     }
@@ -39,7 +40,7 @@ const loadingCharactersObject = (characters) => {
     return charactersArray;
 }
 
-const createHTMLElements = (character) => {
+const createHTMLCharactersElements = (character) => {
     const characterContainer = document.createElement('div');
     const name = document.createElement('h3');
     const status = document.createElement('p');
@@ -53,14 +54,14 @@ const createHTMLElements = (character) => {
     location.textContent = `Location: ${character.location}`;
     img.src = character.image;
 
-    addingClasses(characterContainer, name, status, species, location);
+    addingClassesToCharacters(characterContainer, name, status, species, location);
 
     characterContainer.append(img, name, status, species, location);
 
     return characterContainer;
 }
 
-const addingClasses = (characterContainer, name, status, species, location) => {
+const addingClassesToCharacters = (characterContainer, name, status, species, location) => {
     characterContainer.className = 'character-container';
     name.className = 'name';
     status.className = 'status';
@@ -94,7 +95,7 @@ function intervalTrigger()  {
         if(!myBoolean){
             window.clearInterval(interval); 
         }
-    }, 0);
+    }, 75);
 };
 
 document.querySelector("#obtainEP").addEventListener("click", async() => {
@@ -104,7 +105,7 @@ document.querySelector("#obtainEP").addEventListener("click", async() => {
     const data = await response.json();
     const episodes = await data.episodes;
     let episodesArray = [];
-
+    console.log(episodes)
     changingLoadingMessage("episodes");
     
     const responseEpisodes = await fetch(episodes);
@@ -112,11 +113,48 @@ document.querySelector("#obtainEP").addEventListener("click", async() => {
     const obtainedEpisodes = await function (dataEpisodes) {                
         episodesArray = loadingEpisodesObject(dataEpisodes.results);
         for(let i = 0; i < episodesArray.length; i++){
-            fragment.append(createHTMLElements(episodesArray[i]));
+            fragment.append(createHTMLEpisodesElements(episodesArray[i]));
             episodesContainer.append(fragment);
         }
     }
     obtainedEpisodes(dataEpisodes);    
 })
 
+const loadingEpisodesObject = (episodes) => {
+    let episodesArray = [];
+    for(let episode in episodes){
+        episodesArray[episode] = new Episodes(episodes[episode].id, episodes[episode].air_date, episodes[episode].episode, episodes[episode].name);
+    }
 
+    return episodesArray;    
+}
+
+const createHTMLEpisodesElements = (episode) => {
+    const episodeContainer = document.createElement('div');
+    const name = document.createElement('h3');
+    const episodeName = document.createElement('p');
+    const air_date = document.createElement('p');
+    const id = document.createElement('p');
+
+    name.textContent = episode.name;
+    id.textContent = `ID: ${episode.id}`;
+    episodeName.textContent = `Episode: ${episode.episode}`;
+    air_date.textContent = `Air date: ${episode.air_date}`;
+
+    addingClassesToEpisodes(episodeContainer, name, id, air_date, episodeName);
+
+    episodeContainer.append(name, id, episodeName, air_date);
+
+    return episodeContainer;
+}
+
+const addingClassesToEpisodes = (episodesContainer, name, id, air_date, episodeName) => {
+    episodesContainer.className = 'episodes-container';
+    name.className = 'name';
+    id.className = 'id';
+    air_date.className = 'air_date';
+    episodeName.className = 'episodeName';
+    if(name.textContent.length > 20){
+        name.className += ' largeName';
+    }
+}
