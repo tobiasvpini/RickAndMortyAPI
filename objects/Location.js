de2c2f3a429aa72e72ab9777b7f0fdf0d12b9e5d
Tobias <tobiasvpini@gmail.com>
2021-05-31 22:59:23 +0200

    Object Location created

---
+++ objects/Location.js
@@ -0,0 +1,9 @@
+export default class Location{
+    constructor(id, name, type, dimension, residents){
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.dimension = dimension;
+        this.residents = residents;
+    }
+}
